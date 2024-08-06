0642d83e42daed520b2084de59f81514a682c11c
dijelivareka <dl29359@seeu.edu.mk>
2024-08-07 00:31:01 +0200

    Add PaymentService for payment gateway integration

---
+++ ECommerceProject/Services/PaymentServices.cs
@@ -0,0 +1,11 @@
+namespace ECommerceProject.Services
+{
+    public class PaymentService
+    {
+        public bool ProcessPayment(string paymentDetails)
+        {
+            // Logic to interact with payment gateway API
+            return true;
+        }
+    }
+}
