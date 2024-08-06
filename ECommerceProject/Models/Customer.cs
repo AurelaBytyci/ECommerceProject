c9c07fed6e83501f4bca3141f89e844610ac5429
AurelaBytyci <ab29360@seeu.edu.mk>
2024-08-07 00:04:14 +0200

    Customer class is created

---
+++ ECommerceProject/Models/Customer.cs
@@ -0,0 +1,11 @@
+namespace ECommerceProject.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
