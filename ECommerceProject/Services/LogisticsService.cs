ad29b7beab0041d993ab84fdbed03068d45186ff
dijelivareka <dl29359@seeu.edu.mk>
2024-08-07 00:32:44 +0200

    Add LogisticsService for logistics integration

---
+++ ECommerceProject/Services/LogisticsService.cs
@@ -0,0 +1,12 @@
+namespace ECommerceProject.Services
+{
+    public class LogisticsService
+    {
+        public bool ArrangeShipment(string shipmentDetails)
+        {
+            // Logic to interact with logistics API
+            return true;
+        }
+    }
+
+}
