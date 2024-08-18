c71ea3aae9e79f8eebd64fb45749478cba71ac7a
AurelaBytyci <ab29360@seeu.edu.mk>
2024-08-18 13:30:44 +0200

    LogisticsService updated

--- ECommerceProject/Services/LogisticsService.cs
+++ ECommerceProject/Services/LogisticsService.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace ECommerceProject.Services
 {
-    public class LogisticsService
+    public interface ILogisticsService
+    {
+        bool ArrangeShipment(string shipmentDetails);
+        Task<string> GetOrderStatus(int orderId);
+    }
+
+    public class LogisticsService : ILogisticsService
     {
         public bool ArrangeShipment(string shipmentDetails)
         {
@@ -32,6 +39,24 @@ namespace ECommerceProject.Services
             }
         }
 
+        public async Task<string> GetOrderStatus(int orderId)
+        {
+            // Logic to retrieve the order status from a third-party API
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"https://logisticsapi.com/api/shipments/{orderId}/status");
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    LogError(response.ReasonPhrase);
+                    return "Error retrieving order status";
+                }
+            }
+        }
+
         private string SerializeDetails(string details)
         {
             return JsonConvert.SerializeObject(details);
@@ -47,6 +72,7 @@ namespace ECommerceProject.Services
 
         private void SaveTrackingInfo(string trackingInfo)
         {
+            // Save tracking information to the database or another storage system
         }
 
         private void LogError(string errorMessage)
