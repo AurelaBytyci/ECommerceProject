c97120de1cf93835662b2586227a79a2bdd65324
AurelaBytyci <ab29360@seeu.edu.mk>
2024-08-18 13:48:30 +0200

    ApplicationDbContext updated

--- ECommerceProject/Models/ApplicationDbContext.cs
+++ ECommerceProject/Models/ApplicationDbContext.cs
@@ -4,16 +4,14 @@ namespace ECommerceProject.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<Product> Products { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
-      
-  
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
